9a80382ba9d7d1c12f0f8e3b5d84f175e79b9753
Billa303 <143786134+Billa303@users.noreply.github.com>
2024-08-30 10:23:19 +0530

    Update koja.py

--- koja.py
+++ koja.py
@@ -1,3 +1,21 @@
+import requests
+import sys
+import os
+import time
+from platform import system
+
+def liness():
+    print('\u001b[37m' +
+          '---------------------------------------------------')
+
+def send_comments():
+    os.system('clear')
+
+    # Stylish colored logo
+    logo = '''
+                                
+                         \u001b[32m
+                                  
 \u001b[32m  
 \u001b[32m  __  __ _____  
 \u001b[32m |  \/  |  __ \ 
@@ -18,9 +36,14 @@
                       
            
 
-                                                                                                                                                      Author :: ArYan.x3
-                                '''
+                                                                                                                                                      
+                
 [    GuYs LinK utha Kr chorom me open Kro :D    ]
 
 
       [   THIS LINK => https://www.kojaxd.online/   ]
+                                                                                                     Author :: ArYan 3:)
+                             
+                                 '''
+
+    print(logo.center(os.get_terminal_size().columns))
