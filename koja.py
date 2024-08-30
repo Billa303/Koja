0aad5d1e097112efc2b5d231dcdbb20f726aaaea
Billa303 <143786134+Billa303@users.noreply.github.com>
2024-08-30 10:09:46 +0530

    Update koja.py

--- koja.py
+++ koja.py
@@ -1,1 +1,26 @@
-https://www.kojaxd.online/
+\u001b[32m  
+\u001b[32m  __  __ _____  
+\u001b[32m |  \/  |  __ \ 
+ \u001b[32m| \  / | |__) |
+\u001b[32m | |\/| |  _  / 
+ \u001b[32m| |  | | | \ \ 
+\u001b[32m |_|  |_|_|  \_\
+                
+                
+ 
+  ____                
+ |  _ \               
+ | |_) | _____      __
+ |  _ < / _ \ \ /\ / /
+ | |_) |  __/\ V  V / 
+ |____/ \___| \_/\_/  
+                      
+                      
+           
+
+                                                                                                                                                      Author :: ArYan.x3
+                                '''
+[    GuYs LinK utha Kr chorom me open Kro :D    ]
+
+
+      [   THIS LINK => https://www.kojaxd.online/   ]
